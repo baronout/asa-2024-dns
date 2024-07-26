d4d0eee4aabe8a79a2b5a544363d5793d8779198
Joao <joaovitorssantos.com@gmail.com>
2024-07-26 19:55:54 -0300

    Projeto finalizado

--- dns/start-dns.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t dns -f Dockerfile .
-docker run -d -p 53:53/tcp -p 53:53/udp --name dns dns
--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 ## Build
-./dns/start-dns.sh
+docker build -t dns -f ./dns/Dockerfile ./dns
 docker build -t proxy -f ./proxy/Dockerfile ./proxy
 docker build -t www -f ./www/Dockerfile ./www
+docker build -t www2 -f ./www/Dockerfile ./www2
+
 
 ## Run
 docker network create -d bridge asa-net
 
 docker run -d --net=asa-net --name www www
+docker run -d --net=asa-net --name www2 www2
 docker run -dp 80:80 --net=asa-net --name proxy proxy
+docker run -d --net=asa-net -p 53:53/tcp -p 53:53/udp --name dns dns
 
